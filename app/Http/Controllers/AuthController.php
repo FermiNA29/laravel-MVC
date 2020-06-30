5085e7799cd0631094860bde8b1e3201f25cb8ef
FermiNA29 <58778153+FermiNA29@users.noreply.github.com>
2020-06-30 23:01:17 +0700

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register() {
+        return view('form');
+    }
+
+    public function wellcome(Request $request) {
+        $name = $request->all();
+        // dd($name);
+        return view('wellcome', ['name' => $name]);
+    }
+}
